08b6a7c5bd87b045777dfb2a0917f768b9feb479
Gurman Singh <gurmansingh57801@gmail.com>
2026-05-30 17:44:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch("https://dummyjson.com/users")
 function displayUsers(data) {
     const list = document.getElementById("list"); 
     list.innerHTML = "";
-
+ 
     data.forEach(user => {
         let li = document.createElement("li");
         li.textContent = user.firstName + " " + user.lastName;
